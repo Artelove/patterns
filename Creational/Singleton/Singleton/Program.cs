166429c486c1c473a72e63b938283c0990421707
alexandr karagichev <arteevil.off@gmail.com>
2024-10-21 17:19:01 +0300

    Patterns - add singleton

---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.Instance.Log("213");
+        Logger.Instance.Log("123");
+    }
+
+}
+
+//Не потокобезопаская реализация.
+public class Logger
+{
+    private static Logger? _instance;
+
+    public static Logger Instance
+    {
+        get
+        {
+            if (_instance is not null)
+            {
+                return _instance;
+            }
+            
+            _instance = new Logger();
+            return _instance;
+        }
+    }
+
+    private Logger() { }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+//Потокобезопаская реализация через статический конструктор.
+public sealed class LoggerSave1
+{
+    public static LoggerSave1 Instance { get; private set; } = new LoggerSave1();
+
+    // Статический конструктор гарантирует, что экземпляр будет создан только один раз
+    static LoggerSave1() { }
+
+    private LoggerSave1() { }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+
